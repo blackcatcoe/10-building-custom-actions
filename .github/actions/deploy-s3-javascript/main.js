dc52e6e2070c8e1038fe6d39d499231c808a7b2b
Itsara Thongsri <itsara.th@kbtg.tech>
2023-10-30 16:03:08 +0700

    test javascript

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,18 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
-    core.notice('Hello from my custom JavaScript action!');
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // 2) Log some debug info
+    core.notice(`Deploying ${distFolder} to ${bucket} in ${bucketRegion}`);
+
+    // 3) Run the AWS CLI command
+    // const s3Uri = `s3://${bucket}`
+    // exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+    // core.notice('Hello from my custom JavaScript action!');
 }
 
 run();
